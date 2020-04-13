9e94605bf59bb05b8bd23498c2a49940e42d8092
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2020-04-13 00:58:08 -0300

    Trabajando con ciclos for y while

---
+++ src/EjemploCiclosFor.java
@@ -0,0 +1,11 @@
+
+public class EjemploCiclosFor {
+
+	public static void main(String[] args) {
+		
+		//   variable           condicion      ejecutar al FINAL
+		for (int contador = 0; contador <=10 ; contador++) {
+			System.out.println(contador);
+		}
+	}
+}
---
+++ src/EjercicioMatriz.java
@@ -0,0 +1,16 @@
+
+public class EjercicioMatriz {
+
+	public static void main(String[] args) {
+		
+		for(int fila = 0; fila <= 10; fila++) {	
+			for (int columna = 0; columna < fila; columna++) {
+				System.out.print("*");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+}
