cc25182c8b7e5387152a7eebbc153ae055e3a147
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2020-04-13 00:52:15 -0300

    Trabajando con caracteres

---
+++ src/EjemploValoresVariables.java
@@ -0,0 +1,20 @@
+
+public class EjemploValoresVariables {
+
+	
+	public static void main(String[] args) {
+		
+		int numero1 = 5;
+		int numero2 = 9;
+		
+		System.out.println(numero2);
+		
+		numero2 = numero1; // 5
+		System.out.println(numero2);
+		
+		numero1 = 88;
+		// numero2 = ?????
+		System.out.println(numero2);
+	}
+	
+}
